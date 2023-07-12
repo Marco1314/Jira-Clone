9a213b7e023a18d831a87da1df8bf8568240d0d5
Marco1314 <MarcoHadzija@gmx.at>
2023-07-12 14:10:33 +0200

    commit

--- view/index.view.php
+++ view/index.view.php
@@ -1,7 +1,12 @@
-<div class="logn">
-    <input type="text" name="vname" />
-    <input type="text" name="nname" />
-    <input type="email" name="email" />
-    <input type="password" name="password" />
-
+<div class="container">
+    <div class="row">
+        <div class="col-6 mx-auto">
+            <div class="login">
+                <input type="text" name="vname" />
+                <input type="text" name="nname" />
+                <input type="email" name="email" />
+                <input type="password" name="password" />
+            </div>
+        </div>
+    </div>
 </div>
